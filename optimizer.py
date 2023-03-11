8767fad7097ac8a538ec4f2884c599594a0e9830
Mochammad Zidan Ardany <78547378+zidanardany@users.noreply.github.com>
2023-03-12 06:59:30 +0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -9,6 +9,8 @@ import optimizers.PSO as pso
 import optimizers.MVO as mvo
 import optimizers.GWO as gwo
 import optimizers.mGWO as mgwo
+import optimizers.iGWO as mgwo
+import optimizers.wGWO as mgwo
 import optimizers.MFO as mfo
 import optimizers.CS as cs
 import optimizers.BAT as bat
@@ -51,6 +53,10 @@ def selector(algo, func_details, popSize, Iter):
     elif algo == "GWO":
         x = gwo.GWO(getattr(benchmarks, function_name), lb, ub, dim, popSize, Iter)
     elif algo == "mGWO":
+        x = mgwo.mGWO(getattr(benchmarks, function_name), lb, ub, dim, popSize, Iter)
+    elif algo == "iGWO":
+        x = mgwo.mGWO(getattr(benchmarks, function_name), lb, ub, dim, popSize, Iter)
+    elif algo == "wGWO":
         x = mgwo.mGWO(getattr(benchmarks, function_name), lb, ub, dim, popSize, Iter)
     elif algo == "WOA":
         x = woa.WOA(getattr(benchmarks, function_name), lb, ub, dim, popSize, Iter)
